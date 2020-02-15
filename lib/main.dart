2595a402f3ea3be9c17697904147edd8cf62d661
mauricio <mauricio.hartmann@outlook.com>
2020-02-15 18:25:43 -0300

    Correção no título do app

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Startup Name Generator',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Startup Name Generator'),
